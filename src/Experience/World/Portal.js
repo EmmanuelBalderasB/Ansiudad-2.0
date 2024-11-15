c0d61d20c115dd7e674b0dea84f6c281d1d353fb
Andrew Alvarado <andrew@activetheory.net>
2024-11-14 23:22:13 -0600

    temporal fix for portalColor.png texture error

--- src/Experience/World/Portal.js
+++ src/Experience/World/Portal.js
@@ -28,6 +28,7 @@ export default class Portal {
         this.model.traverse((child) => {
             if (child instanceof THREE.Mesh) {
                 child.castShadow = true
+                child.material = this.material
             }
         })
     }
