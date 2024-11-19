818bc66d7c3a2c70679e37e98a7d9fec1cfb54cb
Emmanuel <ebalderasbeq@gmail.com>
2024-11-18 22:49:10 -0600

    installs vercel devops dev kit

--- src/script.js
+++ src/script.js
@@ -1,4 +1,7 @@
 
 import Experience from './Experience/Experience.js'
-
+import { inject } from "@vercel/analytics"
+import { injectSpeedInsights } from '@vercel/speed-insights';
+inject();
+injectSpeedInsights();
 const experience = new Experience(document.querySelector('canvas.webgl'))
