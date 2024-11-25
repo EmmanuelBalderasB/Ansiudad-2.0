d2460988f0bf33f68594c6cfdaf046524fea548b
Andrew Alvarado <andrew@activetheory.net>
2024-11-24 23:53:13 -0600

    added gaze functionality, mouse code is not clean, should be in its own class

--- src/Experience/Camera.js
+++ src/Experience/Camera.js
@@ -13,6 +13,8 @@ export default class Camera
         this.canvas = this.experience.canvas
         this.appState = this.experience.appState
 
+        this.initMouse()
+
         this.setInstance()
         this.setControls()
 
@@ -25,13 +27,16 @@ export default class Camera
         this.instance.position.set(0, 3, 8)
         this.vectorLookAt = new THREE.Vector3(0, 0, 0)
         this.instance.lookAt(this.vectorLookAt);
-        this.scene.add(this.instance)
+
+        this.gazeWrapper = new THREE.Group()
+        this.gazeWrapper.add(this.instance)
+        this.scene.add(this.gazeWrapper)
     }
 
     setControls()
     {
-        this.controls = new OrbitControls(this.instance, this.canvas)
-        this.controls.enableDamping = true
+        // this.controls = new OrbitControls(this.instance, this.canvas)
+        // this.controls.enableDamping = true
     }
 
     resize()
@@ -67,9 +72,110 @@ export default class Camera
         gsap.to(this.vectorLookAt, { x: 0, y: 0, z: 0, duration: 1.2, ease: "power2.inOut" });
     }
 
+    initMouse() {
+        window.myMouse = {
+            x: 0,
+            y: 0,
+        
+            /* 
+                "smooth" me refiero al valor "suavizado" de las coordenadas X y Y usando la integración de verlet, es una operación matemática que nos ayuda a "acercar" poco a poco la posicion de un objeto de un punto a otro en lugar de reescribir la posición instantaneamente.
+        
+                "cof" o "coeficiente de fricción" es la velocidad con la cual se va a "acercar" del punto A al punto B.
+            */
+            cof: 0.1,
+        
+            smooth: {
+                x: 0,
+                y: 0,
+            },
+        
+        
+            /*
+                "tilt" en este caso me refiero a otro sistema de coordenadas similar al espacio 3D, donde el origen (o coordenada 0,0) es el centro, los positivos son hacia la derecha y arriba, mientras los negativos son hacia la izquierda y abajo. Los extremos de la pantalla son 1 y -1 respectivamente.
+        
+                "tiltSmooth" tiene la misma functión que "smooth", pero usando el valor "normalizado" en lugar del que obtenemos del evento "mousemove"
+            */
+            tilt: {
+                x: 0,
+                y: 0,
+            },
+            
+            tiltSmooth: {
+                x: 0,
+                y: 0,
+            },
+        
+            /*
+                "delta" comunmente es usado para hacer referencia a la "aceleración" actual del movimiento del mouse, cuando estamos en reposo sin mover el cursor su valor es "0". 
+            
+                Para calcularlo necesitamos la posición anterior, de ahí que exista el atributo "last".
+            */
+            last: {
+                x: 0,
+                y: 0,
+            },
+            delta: {
+                x: 0,
+                y: 0,
+            },
+            deltaSmooth: {
+                x: 0,
+                y: 0,
+            },
+        }
+
+        window.addEventListener('mousemove', this.updateMouseCoords.bind(this));
+    }
+    
+    updateMouseCoords(eventData) {
+        myMouse.x = eventData.clientX;
+        myMouse.y = eventData.clientY;
+    
+        this.updateMouseTilt();
+    
+        // console.log(myMouse.x, myMouse.y);
+    }
+
+    updateMouseTilt() {
+        myMouse.tilt.x = ((myMouse.x * 2) - window.innerWidth) / window.innerWidth;
+        myMouse.tilt.y = ((myMouse.y * 2) - window.innerHeight) / window.innerHeight;
+        myMouse.tilt.y *= -1;
+    
+        // console.log(myMouse.tilt.x, myMouse.tilt.y);
+    }
+
+    updateMouseExtraData() {
+        // Actualizando valores "smooth" del mouse
+        myMouse.smooth.x += (myMouse.x - myMouse.smooth.x) * myMouse.cof;
+        myMouse.smooth.y += (myMouse.y - myMouse.smooth.y) * myMouse.cof;
+        
+        // Actualizando valores "tiltSmooth" del mouse
+        myMouse.tiltSmooth.x += (myMouse.tilt.x - myMouse.tiltSmooth.x) * myMouse.cof;
+        myMouse.tiltSmooth.y += (myMouse.tilt.y - myMouse.tiltSmooth.y) * myMouse.cof;
+    
+        // console.log('smooth', myMouse.smooth, myMouse.tiltSmooth);
+    
+    
+        // Actualizando valores "delta" del mouse
+        myMouse.delta.x = myMouse.x - myMouse.last.x;
+        myMouse.delta.y = myMouse.y - myMouse.last.y;
+    
+        myMouse.last.x = myMouse.x;
+        myMouse.last.y = myMouse.y;
+    
+        myMouse.deltaSmooth.x += (myMouse.delta.x - myMouse.deltaSmooth.x) * myMouse.cof;
+        myMouse.deltaSmooth.y += (myMouse.delta.y - myMouse.deltaSmooth.y) * myMouse.cof;
+    }
+
     update()
     {
-        this.controls.update()
+        // this.controls.update()
+        this.updateMouseExtraData();
+        this.gazeWrapper.position.x = -myMouse.tiltSmooth.x * 2;
+        this.gazeWrapper.position.y = -myMouse.tiltSmooth.y * 0.7;
+
         this.instance.lookAt(this.vectorLookAt);
+
+        // this.gazeWrapper.rotateZ(myMouse.deltaSmooth.x * 0.005);
     }
 }
